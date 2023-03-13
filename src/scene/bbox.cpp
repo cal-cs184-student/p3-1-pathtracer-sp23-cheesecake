52b62dbf4ff5c27028fc05e40b01169b2f8dbf3f
reinaw1012 <reina.wang.tw@gmail.com>
2023-03-12 22:15:28 -0700

    part 2 task 2

--- src/scene/bbox.cpp
+++ src/scene/bbox.cpp
@@ -14,8 +14,27 @@ bool BBox::intersect(const Ray& r, double& t0, double& t1) const {
   // If the ray intersected the bouding box within the range given by
   // t0, t1, update t0 and t1 with the new intersection times.
 
+	// Computing tmin and tmax
+	double tx1 = (min.x - r.o.x) / r.d.x;
+	double tx2 = (max.x - r.o.x) / r.d.x;
+	double ty1 = (min.y - r.o.y) / r.d.y;
+	double ty2 = (max.y - r.o.y) / r.d.y;
+	double tz1 = (min.z - r.o.z) / r.d.z;
+	double tz2 = (max.z - r.o.z) / r.d.z;
 
-  return true;
+	Vector3D tmin = Vector3D(std::min(tx1, tx2), std::min(ty1, ty2), std::min(tz1, tz2));
+	Vector3D tmax = Vector3D(std::max(tx1, tx2), std::max(ty1, ty2), std::max(tz1, tz2));
+
+	// Max of min and min of max
+	double max_of_min = std::max(tmin.x, std::max(tmin.y, tmin.z));
+	double min_of_max = std::min(tmax.x, std::min(tmax.y, tmax.z));
+
+	if (max_of_min > min_of_max) return false;
+
+	// Update t0 and t1
+	t0 = max_of_min;
+	t1 = min_of_max;
+	return true;
 
 }
 
